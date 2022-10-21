4f397cd4686d5247a565cf6a072f8e4b68590b23
Cichella <vcichella@uiowa.edu>
2022-10-21 08:38:11 -0500

    added highwire sim

---
+++ Chapter 6 - Root Locus/Highwire Artist/HA_Control_Design.m
@@ -0,0 +1,22 @@
+clear all
+close all
+
+%% Parameters
+s = tf('s');
+J = 10.4;
+l = 1.5;
+L = 2;
+m = 5;
+M = 75;
+g = 9.81;
+
+%% System
+G = 6/(6*m*l^2+2*M*L^2)/(s^2-3*g*(2*m*l+M*L)/(6*m*l^2+2*M*L^2));
+
+
+
+
+
+%% Controller
+C = 3000*(s+2)/(s+5);
+[numC denC] = tfdata(C, 'v');
